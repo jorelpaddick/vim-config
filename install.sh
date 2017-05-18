a8d6b7f3f5aad3587f9e16ddf729115d153105f4
Jorel Paddick <jorelpaddick@gmai.com>
2017-05-18 07:47:19 +0000

    fixed fetch issue

--- install.sh
+++ install.sh
@@ -2,8 +2,8 @@ rm -rf ~/.vim*
 cp .vimrc ~/.vimrc
 cp .vim/ ~/.vim -r
 echo "Getting plugins..."
-git clone https://github.com/jiangmiao/auto-pairs.git ~/.vim/bundle/
-git clone https://github.com/scrooloose/nerdcommenter.git ~/.vim/bundle/
-git clone https://github.com/vim-syntastic/syntastic.git ~/.vim/bundle/
-git clone https://github.com/tpope/vim-surround.git ~/.vim/bundle/
+git clone https://github.com/jiangmiao/auto-pairs.git ~/.vim/bundle/auto-pairs/
+git clone https://github.com/scrooloose/nerdcommenter.git ~/.vim/bundle/nerdcommenter/
+git clone https://github.com/vim-syntastic/syntastic.git ~/.vim/bundle/vim-syntastic/
+git clone https://github.com/tpope/vim-surround.git ~/.vim/bundle/vim-surround/
 echo "Vim has been made awesome"
