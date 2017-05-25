72e84d7b499feb0d2647f7818709fc99ba4d8a73
Jorel Padick <jorelpaddick@gmail.com>
2017-05-25 12:46:23 +0800

    clarified comments

--- install.sh
+++ install.sh
@@ -1,6 +1,15 @@
-rm -rf ~/.vim*
+# Firstly Clean up the old vim config
+mkdir previous-config
+mv *.vim previous-config
+
+# Now copy the new vim files over
 cp .vimrc ~/.vimrc
 cp .vim/ ~/.vim -r
+
+# This config uses additional plugins which have links provided
+# in the README.md file.
+
+# Fetch those plugins from git
 echo "Getting plugins..."
 git clone https://github.com/jiangmiao/auto-pairs.git ~/.vim/bundle/auto-pairs/
 git clone https://github.com/scrooloose/nerdcommenter.git ~/.vim/bundle/nerdcommenter/
