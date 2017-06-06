cc6e8ab80b36067837ecc3dbd4e62f929126e162
Jorel Paddick <jorelpaddick@gmail.com>
2017-06-07 00:59:12 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -16,8 +16,8 @@ git clone https://github.com/jiangmiao/auto-pairs.git ~/.vim/bundle/auto-pairs/
 git clone https://github.com/scrooloose/nerdcommenter.git ~/.vim/bundle/nerdcommenter/
 git clone https://github.com/vim-syntastic/syntastic.git ~/.vim/bundle/vim-syntastic/
 git clone https://github.com/tpope/vim-surround.git ~/.vim/bundle/vim-surround/
-git clone https://github.com/tomtom/tlib_vim.git
-git clone https://github.com/MarcWeber/vim-addon-mw-utils.git
-git clone https://github.com/garbas/vim-snipmate.git
-git clone https://github.com/honza/vim-snippets.git
+git clone https://github.com/tomtom/tlib_vim.git ~/.vim/bundle/tlib_vim/
+git clone https://github.com/MarcWeber/vim-addon-mw-utils.git ~/.vim/bundle/vim-addon-mw-utils/
+git clone https://github.com/garbas/vim-snipmate.git ~/.vim/bundle/vim-snipmate/
+git clone https://github.com/honza/vim-snippets.git ~/.vim/bundle/vim-snippets/
 echo "Vim has been made awesome"
